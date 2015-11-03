a1ee9c59b6fa1e34381a182f376939c7dd5294eb
Greg Narajka <greg.narajka@fanduel.com>
2015-11-03 10:18:14 -0700

    add tests

---
+++ lib/ex_vmstats/backends/test_backend.ex
@@ -0,0 +1,15 @@
+defmodule ExVmstats.TestBackend do
+  require Logger
+
+  def counter(value, metric) do
+    Logger.info "counter #{inspect {value, metric}}"
+  end
+
+  def gauge(value, metric) do
+    Logger.info "gauge #{inspect {value, metric}}"
+  end
+
+  def histogram(value, metric) do
+    Logger.info "histogram #{inspect {value, metric}}"
+  end
+end
