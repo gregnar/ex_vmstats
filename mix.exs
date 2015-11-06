5cc048fc19ad1db3c24f2f70fb0555a181efc945
Greg Narajka <greg.narajka@fanduel.com>
2015-11-06 15:52:58 -0700

    prepare mix.exs and license for hex publishing

--- mix.exs
+++ mix.exs
@@ -1,10 +1,13 @@
 defmodule ExVmstats.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [app: :ex_vmstats,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.1",
+     description: "A generic lib for pushing Erlang VM stats to StatsD.",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -31,5 +34,13 @@ defmodule ExVmstats.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      maintainers: ["Greg Narajka"],
+      licenses: ["Apache 2.0"]
+      links: %{"GitHub" => "https://github.com/fanduel/ex_vmstats"}
+    ]
   end
 end
